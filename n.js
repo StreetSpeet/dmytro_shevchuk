8a1c6fa4fbaf21e7ca692c79d209a6009ecb7c20
DmytroShevchuk <DmSh@.com>
2020-05-27 17:38:55 +0300

    site

---
+++ n.js
@@ -0,0 +1,37 @@
+function openPage(evt, PageName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(PageName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+var elmnt = document.getElementById('timer');
+function birthday(date) {
+  return ((new Date().getTime() - new Date(date)) / (24 * 3600 * 365.25 * 1000)) | 0;
+}
+elmnt.innerHTML=birthday('1997-03-17');
+
+//Language
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
