f6eec3b3c22952092c007db34766baaf96770f08
DmShev <36990849+StreetSpeet@users.noreply.github.com>
2020-09-02 21:54:48 +0300

    sc

--- n.js
+++ n.js
@@ -35,3 +35,15 @@ window.onclick = function(event) {
     }
   }
 }
+//progress-bar
+document.ready(function () {
+    $(".dws-progress-bar").circularProgress({
+        color: "#25ffe4",
+        line_width: 17,
+        height: "350px",
+        width: "350px",
+        percent: 0,
+        // counter_clockwise: true,
+        starting_position: 0
+    }).circularProgress('animate', 100, 2000);
+});
